26d84f4748dc4928b3aa9b5b2566bb389445ec67
Shanthakumar <93003141+shanthakumarvenkatesh@users.noreply.github.com>
2022-04-25 21:38:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// import style from './card.module.css'
+import React, {useState} from 'react'
+const App=()=>{
+        return(
+          // <div className={style.main}>
+          //   <h1>
+          //     Hi There!
+          //   </h1>
+          // </div>
+          <div>
+            <h1>1</h1>
+          
+          <button>clcik</button>
+          </div>
+
+        )
+}
+export default App;
