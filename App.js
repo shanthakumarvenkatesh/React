18acc8168cc863b7cd59a5841133d16a9085849f
Shanthakumar <93003141+shanthakumarvenkatesh@users.noreply.github.com>
2022-04-26 19:56:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 // import style from './card.module.css'
 import React, {useState} from 'react'
 const App=()=>{
+        const [data, setData]=UseState(0);
+        const increment=()=>{
+                setData(data+1)
+        }
         return(
           // <div className={style.main}>
           //   <h1>
           //     Hi There!
           //   </h1>
           // </div>
+        
           <div>
-            <h1>1</h1>
+            <h1>{data}</h1>
           
-          <button>clcik</button>
+          <button onClick{increment}>clcik</button>
           </div>
 
         )
